f2bff60d651315a37b0ab327175217049a174dd5
joff91 <jj256@exeter.ac.uk>
2016-09-13 12:54:14 +0100

    Revert "Bug Fixes"
    This reverts commit decc77397a64dc30033c398cbe66ea5f3d5bff47.

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\OpenSesame\scripts\opensesamerun.exe" "C:\Users\Joff\Documents\OneDrive for Business\OpenSesame\Codebreak.osexp" -l "C:\Users\Joff\Documents\OneDrive for Business\OpenSesame\7937" -s 99 -f
+"C:\Program Files (x86)\OpenSesame\scripts\opensesamerun.exe" "C:\Users\Joff\Documents\OneDrive for Business\OpenSesame\Codebreak.osexp" -l "C:\Users\Joff\Documents\OneDrive for Business\OpenSesame\7937" s- 99 -f
